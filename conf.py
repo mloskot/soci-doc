db1b391fe32a1f332d708625bcbfcd3f08c1e351
Mateusz Loskot <mateusz@loskot.net>
2011-09-26 01:46:57 +0100

    Defined custom html_sidebars in conf.py

--- conf.py
+++ conf.py
@@ -132,6 +132,10 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+html_sidebars = {
+    '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+"index":["sidebar.html",'searchbox.html'],
+"docs":['globaltoc.html',"indexsidebar.html",'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
